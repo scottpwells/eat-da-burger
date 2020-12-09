94a440e400a279547de628fd2ecd0af2e5df14e9
scottpwells <scottpwells@gmail.com>
2020-12-09 18:17:40 -0500

    added controller

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgerControllers.js");
+var routes = require("./controller/burgersController.js");
 
 app.use(routes);
 
@@ -27,3 +27,5 @@ app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+
